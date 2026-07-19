3504569f79e2474f01ff22a00ca671a43555e3e9
Bekir YAZIR <berkeyazir583@gmail.com>
2026-07-20 00:25:53 +0300

    u

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removal) {
+    let newArray = arr.slice();
+    for (let rem of removal) {
+        while(newArray.find(el => el === rem)) {
+            newArray.splice(newArray.indexOf(rem), 1);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
