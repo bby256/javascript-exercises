795d86c69b82cf87cdc41db55a91d21ba20b4880
Bekir YAZIR <berkeyazir583@gmail.com>
2026-07-27 02:29:33 +0300

    u

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    for(let i = 0; i < str.length; i++) {
+        if(str.at(i) !== str.at(-(i + 1))) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
